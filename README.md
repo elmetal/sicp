c1f74d413e1ed906b0a265a5d03e3308141d155f
elmetal <nm7.pupu@gmail.com>
2017-12-17 22:39:39 +0900

    init

---
+++ README.md
@@ -0,0 +1,9 @@
+# sicp
+sicpのメモ
+
+## directory
+### text
+本文のまとめ
+### problem
+問題の回答コード
+
---
+++ problem/1-7.scm
@@ -0,0 +1,16 @@
+(define (average x y)
+    (/ (+ x y) 2))
+
+(define (improve guess x)
+    (average guess (/ x guess)))
+
+(define (sqrt-iter2 guess pre-guess x)
+    (if (good-enough? guess pre-guess)
+        guess
+        (sqrt-iter2 (improve guess x) guess x)))
+
+(define (good-enough? guess pre-guess)
+    (< (/ (abs (- guess pre-guess)) guess) 0.001))
+
+(define (sqrt x)
+    (sqrt-iter2 1.0 100.0 x))
